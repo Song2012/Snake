2c4b44d45e6a1ef2aeb7b94a4ef3b05d3b3b55ad
Song <smj1912@163.com>
2012-03-25 10:39:45 +0800

    create a del_tail method

--- snake.cpp
+++ snake.cpp
@@ -18,6 +18,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cassert>
 using namespace std;
 const int m = 20;
 const int n = 20;
@@ -29,34 +30,47 @@ typedef struct snake {
 	struct snake *next;
 }snake;
 
-snake *head;
-snake *tail;
+struct snake *head;
+struct snake *tail;
 
 void add_head(int a,int b) {
-	snake s;
-	s.x = a;
-	s.y = b;
-	snake *p = &s;
+	struct snake *s = new snake();
+	s->x = a;
+	s->y = b;
+	//game[s->x][s->y] = "#";
 	if(head == NULL) {
-		p->pres = NULL;
-		p->next = NULL;
-		head = p;
-		tail = p;
+		s->pres = NULL;
+		s->next = NULL;
+		head = s;
+		tail = s;
 
 
 	}
 	else {
-		snake *tmp = head;
-		head = p;
-		head->pres = NULL;
-		head->next = tmp;
-		tmp->pres = head;
+		//snake *tmp = head;
+		s->next = head;
+		s->pres = NULL;
+		head->pres = s;
+		head = s;
 	}
 }
 
+void del_tail() {
+	assert(tail != NULL);
+	if(head == tail) {
+		head = tail = NULL;
+	}
+	else {
+		snake *p = tail;
+		tail = tail->pres;
+		tail->next = NULL;
+		delete p;
+	}
 
-void init() {
-	head = tail = NULL;
+}
+
+
+void initMap() {
 	for(int i = 0 ; i != m ; ++i) {
 		game.push_back(vector<string>(n));
 		for(int j = 0 ; j != n ; ++j) {
@@ -70,7 +84,13 @@ void init() {
 	}
 }
 
-void output() {
+void initSnake() {
+	for(snake *p = head ; p != NULL ; p = p->next) {
+		game[p->x][p->y] = "#";
+	}
+}
+
+void draw() {
 	for(int i = 0 ; i != m ; ++i) {
 		for(int j = 0 ; j != n ; ++j){
 			cout<<game[i][j]<<" ";
@@ -79,8 +99,21 @@ void output() {
 	}
 }
 
+void output() {
+	initMap();
+	initSnake();
+	draw();
+}
+
+
+
 int main(void) {
-	init();
+	head = tail = NULL;	
+	add_head(4,5);
+	add_head(5,6);
+	add_head(7,6);
+	output();
+	del_tail();
 	output();
 
 }
